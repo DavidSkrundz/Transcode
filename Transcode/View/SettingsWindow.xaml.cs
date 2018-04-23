e226110ce9bcbf602609e1cc5f45f83dcf688015
David Skrundz <david@skrundz.ca>
2018-04-23 14:30:56 -0600

    Make the settings window actually edit the setting, with error reporting

--- Transcode/View/SettingsWindow.xaml.cs
+++ Transcode/View/SettingsWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using Transcode.Model;
 
 namespace Transcode.View {
@@ -14,6 +16,39 @@ namespace Transcode.View {
 
 			this.settings = settings ?? throw new ApplicationException("new SettingsWindow(): settings is null");
 			this.DataContext = this.settings;
+		}
+
+		private void LocateHandbrakeCLI(object sender, RoutedEventArgs eventArgs) {
+			var dialog = new OpenFileDialog() {
+				Filter = "Executable|*.exe"
+			};
+			if (dialog.ShowDialog() == true) {
+				this.settings.HandbrakePath = dialog.FileName;
+			}
+		}
+
+		private void LocateInputRootPath(object sender, RoutedEventArgs eventArgs) {
+			using (var dialog = new CommonOpenFileDialog()) {
+				dialog.IsFolderPicker = true;
+				dialog.EnsurePathExists = true;
+
+				var result = dialog.ShowDialog();
+				if (result == CommonFileDialogResult.Ok) {
+					this.settings.InputRootPath = dialog.FileName;
+				}
+			}
+		}
+
+		private void LocateOutputRootPath(object sender, RoutedEventArgs eventArgs) {
+			using (var dialog = new CommonOpenFileDialog()) {
+				dialog.IsFolderPicker = true;
+				dialog.EnsurePathExists = true;
+
+				var result = dialog.ShowDialog();
+				if (result == CommonFileDialogResult.Ok) {
+					this.settings.OutputRootPath = dialog.FileName;
+				}
+			}
 		}
 	}
 }
