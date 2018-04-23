116438c82f3bc6dbba249e8a2f73629e427183d3
David Skrundz <david@skrundz.ca>
2018-04-23 14:27:24 -0600

    Add BindingException for VS to ignore

---
+++ Transcode/Exceptions/BindingException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transcode.Exceptions {
+	// Visual Studio should not break on this exception
+	public class BindingException : Exception {
+		public BindingException(string message) : base(message) {}
+	}
+}
