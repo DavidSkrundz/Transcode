4c75a14e4104ff3aee930993221bbc1796112bd7
David Skrundz <david@skrundz.ca>
2018-05-04 17:59:23 -0600

    Add a window to edit items

---
+++ Transcode/View/EditItemWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using Transcode.ViewModel;
+
+namespace Transcode.View {
+	/// <summary>
+	/// Interaction logic for EditItemWindow.xaml
+	/// </summary>
+	public partial class EditItemWindow : Window {
+		private ItemViewModel itemViewModel;
+
+		public EditItemWindow(ItemViewModel itemViewModel) {
+			InitializeComponent();
+
+			this.itemViewModel = itemViewModel ?? throw new ApplicationException("new EditItemWindow(): itemViewModel is null");
+			this.DataContext = this.itemViewModel;
+		}
+
+		private void DoneButtonClicked(object sender, RoutedEventArgs eventArgs) {
+			if (this.itemViewModel.Item.PresetName.Length == 0) { return; }
+			this.Close();
+		}
+	}
+}
